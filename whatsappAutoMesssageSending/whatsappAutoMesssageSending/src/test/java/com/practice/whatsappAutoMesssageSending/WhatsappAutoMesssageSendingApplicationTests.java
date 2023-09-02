59d50ba425ed2d67af6e6a46183f60a1f8f0c9ee
Ivan Tang <ivantang1997@gmail.com>
2023-09-02 18:28:03 +0800

    external lib

--- whatsappAutoMesssageSending/whatsappAutoMesssageSending/src/test/java/com/practice/whatsappAutoMesssageSending/WhatsappAutoMesssageSendingApplicationTests.java
+++ whatsappAutoMesssageSending/whatsappAutoMesssageSending/src/test/java/com/practice/whatsappAutoMesssageSending/WhatsappAutoMesssageSendingApplicationTests.java
@@ -3,6 +3,7 @@ package com.practice.whatsappAutoMesssageSending;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class WhatsappAutoMesssageSendingApplicationTests {
 
