63cfec79a5a213b4830f2bac7529061b009eacd7
ivantang1997 <136788383+ivantang1997@users.noreply.github.com>
2023-09-02 18:08:38 +0800

    Add files via upload

---
+++ whatsappAutoMesssageSending/whatsappAutoMesssageSending/src/main/java/com/practice/whatsappAutoMesssageSending/WhatsappAutoMesssageSendingApplication.java
@@ -0,0 +1,13 @@
+package com.practice.whatsappAutoMesssageSending;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsappAutoMesssageSendingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsappAutoMesssageSendingApplication.class, args);
+	}
+
+}
---
+++ whatsappAutoMesssageSending/whatsappAutoMesssageSending/src/test/java/com/practice/whatsappAutoMesssageSending/WhatsappAutoMesssageSendingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.whatsappAutoMesssageSending;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappAutoMesssageSendingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
